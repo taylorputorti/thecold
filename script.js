8806aff88e7277cc6ae27ed0dbe969a86f162a0b
taylorsawft <tputorti@transportationone.com>
2018-04-15 18:02:56 -0500

    tree comments added

--- script.js
+++ script.js
@@ -11,26 +11,50 @@ $(document).ready(function(){
     textblock.append('<p>it\'s snowing outside</p>');
     command.append('<button class="btn move">move to window?</button> <button class="btn stay">stay put</button>');
 
+//TREE A
     $('.move').click(function(){
       clear();
       textblock.append('<p>you see a mysterious shape moving in the distance</p>');
       command.append('<button class="move btn">check your door is locked</button> <button class="stay btn">remain still and try to get a closer look</button>');
+      //DECISION 1A
       $('.stay').click(function(){
         clear();
-        textblock.append('<p>the shambling figure draws closer, wearing clothing unappropriate for the weather</p><p>you think you may recognize the figure</p>');
-    }) })
+        textblock.append('<p>the shambling figure draws closer, wearing clothing unappropriate for the weather</p><p>you think you may recognize them</p>');
 
+      }) // decision 1a
+      //DECISION 1B
+      $('.move').click(function(){
+        clear();
+        textblock.append('<p>you rush to your door</p> <p>it was open</p><p>you quickly lock the deadbolt</p>');
+        command.append('<button class="move btn">fetch your shotgun</button><button class="stay btn">return to the window</button>');
+        //DECISION 2A
+        $('.move').click(function(){
+          clear();
+          textblock.append('<p>you move into the kitchen to grab your gun</p><p>in the window over the sink, you see more shadowy figures climbing the hill behind your house</p>');
+        }) // decision 2a
+        //  DECISION 2B
+        $('.stay').click(function(){
+
+        }) // decision 2b
+      }) //decision 1b
+    }) // tree a
+
+//TREE B
     $('.stay').click(function(){
       clear();
       textblock.append('<p>the wind howls, your house creaks</p>');
       command.append('<button class="move btn">move to window?</button> <button class="btn stay">stay put</button>');
+      //DECISION 1
       $('.move').click(function(){
         clear();
         textblock.append('<p>you see a mysterious shape moving in the distance</p>');
         command.append('<button class="move btn">check your door is locked</button> <button class="stay btn">remain still and try to get a closer look</button>');
+        //DECISION 2
         $('.stay').click(function(){
           clear();
-          textblock.append('<p>the shambling figure draws closer, wearing clothing unappropriate for the weather</p><p>you think you may recognize the figure</p>');
-      })})})
-  })
-})
+          textblock.append('<p>the shambling figure draws closer, wearing clothing unappropriate for the weather</p><p>you think you may recognize them</p>');
+      }) //decision 2
+    }) //decision 1
+    }) //tree b
+  }) // start
+}) // doc ready
