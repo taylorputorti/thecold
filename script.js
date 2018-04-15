d6591f797642f5634fdb9503a347c58ef6909329
taylorsawft <tputorti@transportationone.com>
2018-04-15 17:13:53 -0500

    first decision tree

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
   var textblock = $('#events');
   var command = $('#commands');
+  var clear = function(){
+    textblock.empty();
+    command.empty();
+  }
 
   $('#start').click(function(){
     command.empty();
     textblock.append('<p>it\'s snowing outside</p>');
-    command.append('<button id="window" class="btn">move to window?</button> <button class="btn stay">stay put</button>');
-    $('#window').click(function(){
-      textblock.empty();
-      command.empty();
+    command.append('<button class="btn move">move to window?</button> <button class="btn stay">stay put</button>');
+
+    $('.move').click(function(){
+      clear();
       textblock.append('<p>you see a mysterious shape moving in the distance</p>');
+
+    })
+
+    $('.stay').click(function(){
+      clear();
+      textblock.append('<p>the wind howls, your house creaks</p>');
+      command.append('<button class="move btn">move to window?</button> <button class="btn stay">stay put</button>');
+      $('.move').click(function(){
+        clear();
+        textblock.append('<p>you see a mysterious shape moving in the distance</p>');
+      })
     })
   })
 })
