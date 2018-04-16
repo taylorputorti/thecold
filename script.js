f13c9f6f552e7693fd42e6c21ae03bf38061937d
taylorsawft <tputorti@transportationone.com>
2018-04-15 21:38:35 -0500

    tree 3 finished

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ $(document).ready(function(){
     command.empty();
   }
 
+  //command.append('<button class="move btn"></button><button class="stay btn"></button>')
+
   $('#start').click(function(){
     command.empty();
-    textblock.append('<p>it\'s started snowing outside</p><p>you can see the flurries out your living room window</p><p>strange things have been happening since the first snow</p><p>people going missing and disturbing noises in the night</p>');
-    command.append('<button class="btn move">move to window?</button> <button class="btn stay">stay put</button>');
+    textblock.hide();
+    textblock.append('<p>it has started snowing</p><p>you can see the flurries outside your living room window</p><p>strange things have been happening since winter started</p><p>people going missing and disturbing noises in the night</p>');
+    command.append('<button class="btn move">go to window?</button> <button class="btn stay">stay put</button>');
+    textblock.fadeIn(900, 'linear');
 
     //TREE A
     $('.move').click(function(){
@@ -19,22 +23,35 @@ $(document).ready(function(){
       //DECISION 1A
       $('.stay').click(function(){
         clear();
-        textblock.append('<p>the shambling figure draws closer, wearing clothing unappropriate for the weather</p><p>you think you may recognize them</p>');
+        textblock.append('<p>the shambling figure draws closer, wearing clothing inapropriate for the weather</p><p>you think you may recognize this person</p>');
+        command.append('<button class="move btn">fetch your shotgun</button><button class="stay btn">remain by the window</button>');
+        //DECISION 2A
+        $('.move').click(function(){
+          clear();
+          textblock.append('<p>you move into the kitchen and grab your gun</p><p>in the window over the sink, you see more shadowy figures climbing the hill behind your house</p>');
+          command.append('<button class="move btn">barricade your windows</button><button class="stay btn">try to get a closer look</button>');
+        }) // decision 2a
+        //DECISION 2B
+        $('.stay').click(function(){
 
+        }) //decision 2b
       }) // decision 1a
       //DECISION 1B
       $('.move').click(function(){
         clear();
-        textblock.append('<p>you rush to your door</p> <p>it was open</p><p>you quickly lock the deadbolt</p>');
+        textblock.append('<p>you rush to your door</p><p>it was open</p><p>you quickly lock the deadbolt</p>');
         command.append('<button class="move btn">fetch your shotgun</button><button class="stay btn">return to the window</button>');
         //DECISION 2A
         $('.move').click(function(){
           clear();
-          textblock.append('<p>you move into the kitchen to grab your gun</p><p>in the window over the sink, you see more shadowy figures climbing the hill behind your house</p>');
+          textblock.append('<p>you move into the kitchen and grab your gun</p><p>in the window over the sink, you see more shadowy figures climbing the hill behind your house</p>');
+          command.append('<button class="move btn">barricade your windows</button><button class="stay btn">try to get a closer look</button>');
         }) // decision 2a
         //  DECISION 2B
         $('.stay').click(function(){
-
+          clear();
+          textblock.append('<p>you walk back towards the window</p><p>the figure has drawn closer</p><p>you can see it is missing an arm and part of its face</p>');
+          command.append('<button class="move btn">take cover upstairs</button><button class="stay btn">barricade the window</button>');
         }) // decision 2b
       }) //decision 1b
     }) // tree a
@@ -43,7 +60,7 @@ $(document).ready(function(){
     $('.stay').click(function(){
       clear();
       textblock.append('<p>the wind howls, your house creaks</p><p>actually... there might be another sound outside</p><p>it\'s not a sound you\'ve heard before</p>');
-      command.append('<button class="move btn">move to window?</button> <button class="btn stay">stay put</button>');
+      command.append('<button class="move btn">go to window?</button> <button class="btn stay">stay put</button>');
       //DECISION 1A
       $('.move').click(function(){
         clear();
@@ -51,7 +68,9 @@ $(document).ready(function(){
         command.append('<button class="move btn">check your door is locked</button> <button class="stay btn">remain still and try to get a closer look</button>');
         //DECISION 2A
         $('.move').click(function(){
-
+          clear();
+          textblock.append('<p>you rush to your door</p><p>it was open</p><p>you quickly lock the deadbolt</p>');
+          command.append('<button class="move btn">fetch your shotgun</button><button class="stay btn">return to the window</button>');
         })// decision 2a
         //DECISION 2B
         $('.stay').click(function(){
@@ -66,7 +85,23 @@ $(document).ready(function(){
         command.append('<button class="move btn">move to window?</button> <button class="btn stay">stay put</button>');
         //DECISION 2A
         $('.move').click(function(){
-
+          clear();
+          textblock.append('<p>a shambling figure approaches</p><p>you can see it is missing an arm and part of its face</p><p>how is it still moving?</p>');
+          command.append('<button class="move btn">check your door is locked</button><button class="stay btn">barricade the window</button>');
+          //DECISION 3A
+          $('.move').click(function(){
+            clear();
+            textblock.append('<p>you rush to your door</p><p>it was open</p><p>you quickly lock the deadbolt</p>');
+            command.append('<button class="move btn">fetch your shotgun</button><button class="stay btn">barricade your window</button>');
+          })//decision 3a
+          //DECISION 3B
+          $('.stay').click(function(){
+            clear();
+            textblock.append('<p>you push your bookcase in front of the window</p><p>unfortunately, the door was unlocked</p><p>your front door bursts open</p><p>the creature shambles inside</p><p>you recognize it as mr. timmons, the gardener from down the hill who died last week</p><p>you barely have the chance to scream before he starts to devour you</p>');
+            command.append('<button class="btn refresh">try again?</button>');
+            $('.refresh').click(function(){
+              location.reload(); })
+          })//decision 3b
         })//decision 2a
         //DECISION 2B
         $('.stay').click(function(){
